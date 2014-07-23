0a9ddbb5388dd894b5defdbdebd6e48a53e08512
Fabrice Weinberg <Fabrice@weinberg.me>
2014-07-23 17:23:04 +0200

    build

--- dist/interpolate.js
+++ dist/interpolate.js
@@ -8,96 +8,18 @@
     root.interpolate = factory();
   }
 })(this, function () {
-
-  'use strict';
-
   /**
-   * @class Interpolate
-   * @classdesc Interpolates a String against an Object's values
-   * @param {String} tmpl Template to store for parsing
-   */
-  function Interpolate (tmpl) {
-    if (getType(tmpl) !== 'String') return;
-    this.template = tmpl;
-  }
-
-  /**
-   * @name Interpolate#getDelimiters
    * @desc Match content between delimiters
    * @type {RegExp}
    */
-  Interpolate.getDelimiters = /{{\s*(.+?)\s*}}/g;
+  var getDelimiters = /{{\s*(.+?)\s*}}/g;
 
   /**
-   * @name Interpolate#parse
-   * @desc Parses an Object's values against the stored String template
-   * @returns {String} Parsed template
+   * @desc Parses an template and generates a new function
+   * @returns {Function} function that takes an object as argument and returns a string
    */
-  Interpolate.prototype.parse = function (obj) {
-    if (getType(obj) !== 'Object') return;
-    var temp = this.template;
-    return temp.replace(Interpolate.getDelimiters, function(str, path) {
-      var prop = followPath(obj, path);
-      if(typeof prop === 'undefined') {
-        try {
-          return new Function('obj', 'with(obj) { return ' + path + '; }').apply(obj, [obj]); }
-        catch(err) {
-          throw new Error('Can\'t extract \''+path+'\' from supplied object.');
-        }
-      } else {
-        return prop;
-      }
-    });
-  };
-
-  /**
-   * @name getType
-   * @description Returns the Object's type
-   * @param {Object} item The item to get the type
-   * @returns {String}
-   * @private
-   */
-  function getType (item) {
-    return Object.prototype.toString.call(item).slice(8, -1);
-  }
-
-  /**
-   * @name reduce
-   * @desc Reduce a collection to a single value
-   * @returns {String}
-   * @private
-   */
-  function reduce (collection, iter, initial) {
-    for (var i = 0, length = collection.length; i < length; i++) {
-      initial = iter.call(null, initial, collection[i], i, collection);
-    }
-    return initial;
-  }
-
-  /**
-   * @name followPath
-   * @desc Follows a path on the given data to retrieve a value
-   *
-   * @example
-   * var data = { foo : { bar : "abc" } };
-   * followPath(data, 'foo.bar'); // 'abc'
-   *
-   * @param {Object} data the object to get a value from
-   * @param {String} path a path to a value on the data object
-   * @returns {*} the value of following the path on the data object
-   * @private
-   */
-  function followPath (data, path) {
-    return reduce(path.split('.'), function (prev, curr) {
-      return prev && prev[curr];
-    }, data);
-  }
-
   return function (tmpl) {
-    var template = new Interpolate(tmpl);
-    return function (obj) {
-      return template.parse(obj);
-    };
+    var code = 'with(obj){return \'' +  tmpl.replace(getDelimiters, '\'+($1)+\'') + '\';}';
+    return new Function('obj', code);
   };
-
 });
